9674af21ad292562f4f07050f421d3e1bf166f4c
ManikandanSenthil-Developer <manikandansenthil262@gmail.com>
2025-04-18 03:01:02 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+const App = () => {
+  return (
+    <section className='app'>
+      <Weather/>
+
+    </section>
+  )
+}
+
+export default App
